5f0f22cd105686d1b64cb5c57b5b60d700da99b5
arturof033 <68297760+arturof033@users.noreply.github.com>
2024-09-27 17:47:49 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,64 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+./fib N METHOD
+
+N = positive integer index
+METHOD = 'i' for iterative, 'r' for recursive
+
+Outputs the first N Fibonacci numbers using a memoized version
+of the specified method.
+*/
+
 #define MAX_LEN 1024
-typedef unsigned long long (*FibFunc)(unsigned long long fibIndex);
+typedef unsigned long long (*FibFunc)(unsigned long long);
+FibFunc fib_provider;
 
-
-unsigned long long iter_fib(unsigned long long n) {
+unsigned long long iter_fib(unsigned long long fibIndex) {
     
-    unsigned long long result = 0;
-    unsigned long long value1 = 0;
-    unsigned long long value2 = 1;
+    unsigned long long previous = 0;
+    unsigned long long result = 1;
 
-    for (int i = 1; i <= n; i++)
-    {
-        result = value1 + value2;
-        value2 = value1;
-        value1 = result; 
+    for (int ix = 0; ix <= fibIndex - 2; ix++) {
+        unsigned long long temp = previous + result;
+        previous = result;
+        result = temp;   
     }
 
     return result;
 }
 
-unsigned long long rec_fib(unsigned long long n) {
-    
-    if(n < 2)
-    {
-        return n;
+unsigned long long rec_fib(unsigned long long fibIndex) {
+
+    if(fibIndex < 2) {
+        return fibIndex;
     }
 
-    return rec_fib(n - 1) + rec_fib(n - 2);
+    return (*fib_provider)(fibIndex - 1) + (*fib_provider)(fibIndex - 2);
 }
 
 
-unsigned long long memo_fib(
-    unsigned long long (*fib)(unsigned long long fibIndex), 
-    unsigned long long n) {
+FibFunc chosenFunc;
 
+unsigned long long memo_fib(unsigned long long fibIndex) {
+    
    static unsigned long long memo[MAX_LEN];
 
-    FibFunc chosenFunc = fib;
 
-    if (memo[n] != 0) {
-        return memo[n];
+    if (memo[fibIndex] != 0) {
+        return memo[fibIndex];
     }
 
-    memo[n] = chosenFunc(n);
-    return memo[n];
+    memo[fibIndex] = (*chosenFunc)(fibIndex);
+    return memo[fibIndex];
 }
+
 
 int main(int argc, char * argv[]) {
 
-    unsigned long long N = atoi(argv[1]) - 1;
+    unsigned long long fibIndex = atoi(argv[1]) - 1;
 
-    if (*argv[2] == 'r')
-    {
-        printf("%llu", memo_fib(rec_fib, N));
+    if (*argv[2] == 'r') {
+        chosenFunc = rec_fib;
+        fib_provider = memo_fib;
+        printf("%llu\n", memo_fib(fibIndex));
     }
-    else if (*argv[2] == 'i')
-    {
-        printf("%llu", memo_fib(iter_fib, N));
+    else if (*argv[2] == 'i') {
+        chosenFunc = iter_fib;
+        fib_provider = memo_fib;
+        printf("%llu\n", memo_fib(fibIndex));
     }
     
    return 0;
